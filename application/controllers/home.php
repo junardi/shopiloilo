416dab2c6d51b3e2e08d9b9745d4ea2e316965ac
junardi <mark.boribor73@gmail.com>
2013-09-27 22:04:43 +0800

    some changes

---
+++ application/controllers/home.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	
+	function index() {
+		echo "Hello this is the home page";
+	}
+	
+}
